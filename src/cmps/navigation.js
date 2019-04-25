e334ad3f2cfbdf61cd5b3353605d34ee175a66e8
Claudio Bernal <claudio.mattioli@globant.com>
2019-04-25 13:42:27 +0530

    prepared to start a live session

--- src/cmps/navigation.js
+++ src/cmps/navigation.js
@@ -1,83 +1,83 @@
 const template = document.createElement('template');
 template.innerHTML = `
 <style>
-    :host {
-      display: block;
-      padding: 0;
-      margin: 0;
-      font-family: sans-serif;
-      background: black;
-      width: 200px;
-      color: white;
-      font-size: 14px;
-    }
-    
-    .nav-logo{
-      margin: 18px auto;
-      text-align: left;
-      padding: 0px 12px 12px;
-      box-sizing: border-box;
-    }
+  :host {
+    display: block;
+    padding: 0;
+    margin: 0;
+    font-family: sans-serif;
+    background: black;
+    width: 200px;
+    color: white;
+    font-size: 14px;
+  }
 
-    .nav-logo h1 {
-      display: inline-block;
-      margin: auto 12px;
-      font-size: 1.3em;
-    }
+  .nav-logo{
+    margin: 18px auto;
+    text-align: left;
+    padding: 0px 12px 12px;
+    box-sizing: border-box;
+  }
 
-    .nav-logo .icon-headphones{
-      font-size: 1.3em;
-    }
+  .nav-logo h1 {
+    display: inline-block;
+    margin: auto 12px;
+    font-size: 1.3em;
+  }
 
-    .nav-item, .nav-logo {
-      width: 85%;
-      margin-left:6px;
-    }
+  .nav-logo .icon-headphones{
+    font-size: 1.3em;
+  }
 
-    .nav-placeholder:before {
-      content: '';
-      display: block;
-      border-radius: 5px;
-      width :100%;
-      height: 8px;
-      background: white;
-      opacity: .3;
-      margin: 18px auto;
-      background: linear-gradient(270deg, #ffffff, #9a9a9a, #ffffff);
-      background-size: 200% 200%;
-      animation: RenderAnim 1s linear infinite;
-    }
+  .nav-item, .nav-logo {
+    width: 85%;
+    margin-left:6px;
+  }
 
-    .nav-placeholder.nav-bold:before {
-      opacity:.6;
-      height:14px;
-      margin: 24px auto;
-    }
+  .nav-placeholder:before {
+    content: '';
+    display: block;
+    border-radius: 5px;
+    width :100%;
+    height: 8px;
+    background: white;
+    opacity: .3;
+    margin: 18px auto;
+    background: linear-gradient(270deg, #ffffff, #9a9a9a, #ffffff);
+    background-size: 200% 200%;
+    animation: RenderAnim 1s linear infinite;
+  }
 
-    .nav-underline {
-      border-bottom: 1px solid white;
-    }
+  .nav-placeholder.nav-bold:before {
+    opacity:.6;
+    height:14px;
+    margin: 24px auto;
+  }
 
-    @keyframes RenderAnim { 
-      0%{background-position:0% 0%}
-      50%{background-position:100% 0%}
-      100%{background-position:201% 0%}
-    }
+  .nav-underline {
+    border-bottom: 1px solid white;
+  }
+
+  @keyframes RenderAnim { 
+    0%{background-position:0% 0%}
+    50%{background-position:100% 0%}
+    100%{background-position:201% 0%}
+  }
 </style>
 <link rel="stylesheet" href="/src/styles/icons.css" />
 <nav id="nav-app">
-    <div class="nav-logo nav-underline">
-      <span class="icon-headphones"></span>
-      <h1>ESpofity</h1>
-    </div>
-    <div class="nav-item nav-placeholder nav-bold nav-underline"></div>
-    <div class="nav-item nav-placeholder nav-bold nav-underline"></div>
-    <div class="nav-item nav-placeholder nav-bold"></div>
-    <div class="nav-item nav-placeholder"></div>
-    <div class="nav-item nav-placeholder"></div>
-    <div class="nav-item nav-placeholder"></div>
-    <div class="nav-item nav-placeholder"></div>
-    <div class="nav-item nav-placeholder"></div>
+  <div class="nav-logo nav-underline">
+    <span class="icon-headphones"></span>
+    <h1>ESpofity</h1>
+  </div>
+  <div class="nav-item nav-placeholder nav-bold nav-underline"></div>
+  <div class="nav-item nav-placeholder nav-bold nav-underline"></div>
+  <div class="nav-item nav-placeholder nav-bold"></div>
+  <div class="nav-item nav-placeholder"></div>
+  <div class="nav-item nav-placeholder"></div>
+  <div class="nav-item nav-placeholder"></div>
+  <div class="nav-item nav-placeholder"></div>
+  <div class="nav-item nav-placeholder"></div>
 </nav>
 `;
 
@@ -91,7 +91,7 @@ class NavigationCmp extends HTMLElement {
 
   connectedCallback() {
     if (!window.NavApplication) {
-      import(/* webpackIgnore: true */'/src/dist/NavApplication.js').then(this.renderApp.bind(this));
+      import('/src/dist/NavApplication.js').then(this.renderApp.bind(this));
     } else {
       this.renderApp();
     }
